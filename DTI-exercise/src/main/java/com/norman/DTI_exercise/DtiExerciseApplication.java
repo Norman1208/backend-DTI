79608447849e93f634cc784e76868c82da98c59d
norman1208 <achmadnorman1208@gmail.com>
2024-10-24 20:30:55 +0700

    feat 1: spring boot initialization project

---
+++ DTI-exercise/src/main/java/com/norman/DTI_exercise/DtiExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.norman.DTI_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtiExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtiExerciseApplication.class, args);
+	}
+
+}
---
+++ DTI-exercise/src/test/java/com/norman/DTI_exercise/DtiExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.norman.DTI_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtiExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
